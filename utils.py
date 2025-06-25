440bb7705f2025da7aa37c677cdb98e6548a2a2b
nagisaShiyoto <elon.gilad9@gmail.com>
2025-06-25 13:02:36 +0300

    asd

--- utils.py
+++ utils.py
@@ -4,9 +4,12 @@ from dataclasses import dataclass
 MSG_SIZE = 1024
 EXIT_MSG = "/exit"
 TRANSFER_MSG = "/transfer "
+UNICAST_MSG = "/unicast "
+UNICAST_PARAMS_AMOUNT = 3
 NORMAL_STARTING_SLASH = "//"
 ADMIN_ROOM_NAME = "*"
 ADMIN_PASSWORD = "password"
+
 
 @dataclass
 class user:
